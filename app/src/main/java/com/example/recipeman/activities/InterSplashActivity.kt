b50b5ccb3915f6734a842154b505588a9d5504b0
Daria602 <izotovadaria0602@gmail.com>
2023-05-26 09:37:17 +0300

    Added splash on Login to app

---
+++ app/src/main/java/com/example/recipeman/activities/InterSplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.recipeman.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.recipeman.R
+
+@SuppressLint("CustomSplashScreen")
+class InterSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+}
